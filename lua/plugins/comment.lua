7138b61f77c94e6306ad5fc70eb3e510e105dcc5
Leon <leonsout@outlook.com>
2024-01-08 07:03:08 +0800

    change mini.comment default keymaps

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,11 +1,18 @@
 local M = {
-  -- "numToStr/Comment.nvim",
-  'echasnovski/mini.comment',
-  version = '*',
-  event = "InsertEnter",
-  config = function()
-    -- require("Comment").setup()
-    require("mini.comment").setup()
-  end,
+	-- "numToStr/Comment.nvim",
+	"echasnovski/mini.comment",
+	version = "*",
+	event = "InsertEnter",
+	config = function()
+		-- require("Comment").setup()
+		require("mini.comment").setup({
+			mappings = {
+				comment = "<leader>c",
+				comment_line = "<leader>cc",
+				comment_visual = "<leader>c",
+				textobject = "<leader>c",
+			},
+		})
+	end,
 }
 return M

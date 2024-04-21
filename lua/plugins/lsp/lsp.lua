66810eafaac6c35ef541bc0020fc5b53ea9ad400
Rav4n-T <leonsgmali@gmail.com>
2024-04-22 04:36:03 +0800

    update disagnostic config, disabled unused lsp

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -10,9 +10,17 @@ local M = {
 	opts = {
 		-- options for vim.diagnostic.config()
 		diagnostics = {
-			underline = true,
+			underline = false,
 			update_in_insert = false,
-			virtual_text = { spacing = 2, prefix = ">" },
+			virtual_text = false,
+			-- virtual_text = {
+			-- 	source = "always",
+			-- 	spacing = 2,
+			-- 	prefix = "",
+			-- },
+			float = {
+				source = "always",
+			},
 			severity_sort = true,
 		},
 		-- LSP Server Settings
@@ -39,47 +47,47 @@ local M = {
 					},
 				},
 			},
-			clangd = {
-				cmd = { "clangd", "--header-insertion=iwyu", "--clang-tidy" },
-				root_dir = function(fname)
-					return require("lspconfig.util").root_pattern(
-						".clangd",
-						".clang-tidy",
-						".clang-format",
-						"compile_commands.json",
-						"compile_flags.txt",
-						"configure.ac",
-						".vs",
-						".git"
-					)(fname)
-				end,
-				single_file_support = true,
-			},
-			rust_analyzer = {
-				settings = {
-					["rust-analyzer"] = {
-						imports = {
-							granularity = {
-								group = "module",
-							},
-							prefix = "self",
-						},
-						cargo = {
-							buildScripts = {
-								enable = true,
-							},
-						},
-						procMacro = {
-							enable = true,
-						},
-					},
-				},
-			},
-			emmet_language_server = {
-				init_options = {
-					includeLanguages = { typescriptreact = "html" },
-				},
-			},
+			-- clangd = {
+			-- 	cmd = { "clangd", "--header-insertion=iwyu", "--clang-tidy" },
+			-- 	root_dir = function(fname)
+			-- 		return require("lspconfig.util").root_pattern(
+			-- 			".clangd",
+			-- 			".clang-tidy",
+			-- 			".clang-format",
+			-- 			"compile_commands.json",
+			-- 			"compile_flags.txt",
+			-- 			"configure.ac",
+			-- 			".vs",
+			-- 			".git"
+			-- 		)(fname)
+			-- 	end,
+			-- 	single_file_support = true,
+			-- },
+			-- rust_analyzer = {
+			-- 	settings = {
+			-- 		["rust-analyzer"] = {
+			-- 			imports = {
+			-- 				granularity = {
+			-- 					group = "module",
+			-- 				},
+			-- 				prefix = "self",
+			-- 			},
+			-- 			cargo = {
+			-- 				buildScripts = {
+			-- 					enable = true,
+			-- 				},
+			-- 			},
+			-- 			procMacro = {
+			-- 				enable = true,
+			-- 			},
+			-- 		},
+			-- 	},
+			-- },
+			-- emmet_language_server = {
+			-- 	init_options = {
+			-- 		includeLanguages = { typescriptreact = "html" },
+			-- 	},
+			-- },
 			-- tsserver = {},
 			-- volar = {
 			-- 	filetypes = { "vue" },
@@ -93,7 +101,7 @@ local M = {
 			-- 	},
 			-- },
 			tailwindcss = {},
-			jsonls = {},
+			-- jsonls = {},
 			-- pyright = {},
 			-- cssls = {
 			-- 	filetypes = {
@@ -120,7 +128,12 @@ local M = {
 		-- diagnostics
 		for name, icon in pairs(require("core.options").icons.diagnostics) do
 			name = "DiagnosticSign" .. name
-			vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+			vim.fn.sign_define(name, {
+				text = icon, -- icon | ""
+				texthl = name, -- "name" | ""
+				linehl = "",
+				numhl = "", -- "name" | ""
+			})
 		end
 		vim.diagnostic.config(opts.diagnostics)
 
@@ -129,9 +142,52 @@ local M = {
 		local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 		local ensure_installed = {} ---@type string[]
 
+		local on_attach = function(client, bufnr)
+			vim.api.nvim_create_autocmd("CursorHold", {
+				buffer = bufnr,
+				callback = function()
+					local hover_opts = {
+						focusable = false,
+						close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+						border = "rounded",
+						source = "always",
+						prefix = " ",
+						scope = "cursor",
+					}
+					vim.diagnostic.open_float(nil, hover_opts)
+				end,
+			})
+
+			if client.resolved_capabilities.document_highlight then
+				vim.cmd([[
+          hi! LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+          hi! LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+          hi! LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+        ]])
+				vim.api.nvim_create_augroup("lsp_document_highlight", {
+					clear = false,
+				})
+				vim.api.nvim_clear_autocmds({
+					buffer = bufnr,
+					group = "lsp_document_highlight",
+				})
+				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+					group = "lsp_document_highlight",
+					buffer = bufnr,
+					callback = vim.lsp.buf.document_highlight,
+				})
+				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+					group = "lsp_document_highlight",
+					buffer = bufnr,
+					callback = vim.lsp.buf.clear_references,
+				})
+			end
+		end
+
 		local function setup(server)
 			local server_opts = vim.tbl_deep_extend("force", {
 				capabilities = vim.deepcopy(capabilities),
+				on_attach = on_attach,
 			}, servers[server] or {})
 			require("lspconfig")[server].setup(server_opts)
 		end

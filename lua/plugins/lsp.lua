ebc46f1c69585df800ce62c754b8b812d80b4c66
Rav4n-T <leonsgmali@gmail.com>
2024-05-05 19:29:29 +0800

    add barecue plugin and add rust lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,38 @@ return {
 			{ "williamboman/mason.nvim" },
 			{ "williamboman/mason-lspconfig.nvim" },
 			{ "hrsh7th/cmp-nvim-lsp" },
+			{
+				"utilyre/barbecue.nvim",
+				name = "barbecue",
+				version = "*",
+				dependencies = {
+					"SmiteshP/nvim-navic",
+					"nvim-tree/nvim-web-devicons", -- optional dependency
+				},
+				config = function()
+					-- triggers CursorHold event faster
+					vim.opt.updatetime = 200
+
+					require("barbecue").setup({
+						create_autocmd = false, -- prevent barbecue from updating itself automatically
+					})
+
+					vim.api.nvim_create_autocmd({
+						"WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+						"BufWinEnter",
+						"CursorHold",
+						"InsertLeave",
+
+						-- include this if you have set `show_modified` to `true`
+						"BufModifiedSet",
+					}, {
+						group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+						callback = function()
+							require("barbecue.ui").update()
+						end,
+					})
+				end,
+			},
 		},
 		---@class PluginLspOpts
 		opts = {
@@ -61,26 +93,26 @@ return {
 				-- 	end,
 				-- 	single_file_support = true,
 				-- },
-				-- rust_analyzer = {
-				-- 	settings = {
-				-- 		["rust-analyzer"] = {
-				-- 			imports = {
-				-- 				granularity = {
-				-- 					group = "module",
-				-- 				},
-				-- 				prefix = "self",
-				-- 			},
-				-- 			cargo = {
-				-- 				buildScripts = {
-				-- 					enable = true,
-				-- 				},
-				-- 			},
-				-- 			procMacro = {
-				-- 				enable = true,
-				-- 			},
-				-- 		},
-				-- 	},
-				-- },
+				rust_analyzer = {
+					settings = {
+						["rust-analyzer"] = {
+							imports = {
+								granularity = {
+									group = "module",
+								},
+								prefix = "self",
+							},
+							cargo = {
+								buildScripts = {
+									enable = true,
+								},
+							},
+							procMacro = {
+								enable = true,
+							},
+						},
+					},
+				},
 				-- volar = {
 				-- 	filetypes = { "vue" },
 				-- 	init_options = {

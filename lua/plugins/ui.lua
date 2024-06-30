376d661c2b1d0e76af262b3db01bb0d5eaefd128
Rav4n-T <leonsgmali@gmail.com>
2024-07-01 07:40:16 +0800

    update noice ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,9 +17,6 @@ return {
 					["cmp.entry.get_documentation"] = true,
 				},
 			},
-			cmdline = {
-				view = "cmdline",
-			},
 			routes = {
 				{
 					filter = {
@@ -45,12 +42,6 @@ return {
 					},
 					opts = { skip = true },
 				},
-			},
-			presets = {
-				bottom_search = true,
-				command_palette = true,
-				long_message_to_split = true,
-				inc_rename = true,
 			},
 		},
 	    -- stylua: ignore

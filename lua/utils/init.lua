161d33ee610100cc136a1aaa82dcce6f7cc52697
Rav4n-T <leonsgmali@gmail.com>
2024-09-17 09:32:18 +0800

    del telescop

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -48,24 +48,6 @@ function M.get_prompt(prompt, default_prompt)
 	return prompt_text
 end
 
-function M.telescope(builtin, opts)
-	local params = { builtin = builtin, opts = opts }
-	return function()
-		builtin = params.builtin
-		opts = params.opts
-		opts = vim.tbl_deep_extend("force", { cwd = M.get_root() }, opts or {})
-		if builtin == "files" then
-			if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
-				opts.show_untracked = true
-				builtin = "git_files"
-			else
-				builtin = "find_files"
-			end
-		end
-		require("telescope.builtin")[builtin](opts)
-	end
-end
-
 M.runCode = function()
 	local ft = vim.bo.filetype
 	vim.cmd("split")

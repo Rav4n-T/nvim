68b936bdda167b9a07a14b3eab3007ba161a3663
Rav4n-T <leonsgmali@gmail.com>
2024-09-17 09:42:33 +0800

    add disable event fu

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -1,6 +1,14 @@
 return {
 	{
 		"nvim-neorg/neorg",
+		cond = function()
+			local note_dir = vim.fn.findfile("index.norg", vim.fn.getcwd() .. ";")
+			if note_dir ~= "" then
+				return true
+			else
+				return false
+			end
+		end,
 		version = "v7.0.0", -- This is the important part!
 		build = ":Neorg sync-parsers",
 		ft = { "norg" },

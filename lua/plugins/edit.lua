0901ce16b87f601f4fe2819a0b32a94712be9e08
Rav4n-T <leonsgmali@gmail.com>
2024-09-17 17:22:52 +0800

    change vim.loop to vim.uv

--- lua/plugins/edit.lua
+++ lua/plugins/edit.lua
@@ -10,20 +10,20 @@ return {
 			local configs = require("nvim-treesitter.configs")
 			---@diagnostic disable-next-line missing-fields
 			configs.setup({
-        ensure_installed = "all",
+				ensure_installed = "all",
 				highlight = {
 					enable = true,
 					disable = function(_, buf) -- first args is lang
 						local max_filesize = 50 * 1024
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
 						if ok and stats and stats.size > max_filesize then
 							return true
 						end
 					end,
 				},
 				indent = {
-          enable = true
-        },
+					enable = true,
+				},
 			})
 		end,
 	},

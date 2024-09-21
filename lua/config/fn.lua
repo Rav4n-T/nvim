f309a2672b715d6636cc33619249453fb054c28d
Rav4n-T <leonsgmali@gmail.com>
2024-09-21 16:29:02 +0800

    add markdown file key q to close with http-client plugin

--- lua/config/fn.lua
+++ lua/config/fn.lua
@@ -124,6 +124,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		"checkhealth",
 		"neotest-summary",
 		"neotest-output-panel",
+		"markdown",
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false

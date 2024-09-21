118b80ec9cad56f631282857c1207fdd4a5dd65d
Rav4n-T <leonsgmali@gmail.com>
2024-09-21 16:25:17 +0800

    del catppuccin colorschema

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -19,31 +19,6 @@ return {
 		end,
 	},
 	{
-		"catppuccin/nvim",
-		event = "VeryLazy",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = true,
-				styles = {
-					comments = { "italic" },
-					conditionals = { "italic" },
-					-- loops = { "bold" },
-					functions = { "italic" },
-					-- keywords = {},
-					-- strings = { "italic" },
-					variables = { "italic" },
-					-- numbers = {},
-					-- booleans = { "bold" },
-					-- properties = { "italic" },
-					-- types = { "bold" },
-					operators = {},
-				},
-			})
-		end,
-	},
-	{
 		"neanias/everforest-nvim",
 		event = "BufEnter",
 		lazy = true,

65dc4d2ac59b2180089e50a8d746bf9bef3cd09c
Rav4n-T <leonsgmali@gmail.com>
2024-09-21 16:26:49 +0800

    change rest plugin
    remove kulala.nvim, add http-clinet.nvim

--- lua/plugins/http.lua
+++ lua/plugins/http.lua
@@ -1,25 +1,13 @@
 return {
 	{
-		"mistweaverco/kulala.nvim",
-		ft = { "http" },
-		opts = {
-			-- split direction
-			-- possible values: "vertical", "horizontal"
-			split_direction = "horizontal",
-			debug = true,
-		},
-		config = function(_, opts)
+		"askfiy/http-client.nvim",
+		config = function()
 			local map = vim.keymap.set
-			local kulala = require("kulala")
-			kulala.setup(opts)
+			local http = require("http-client")
 
-			map("n", "<leader>hr", kulala.run, { desc = "runs the current request" })
-			map("n", "<leader>ha", kulala.run_all, { desc = "runs all request in current buffer" })
-			map("n", "<leader>hl", kulala.replay, { desc = "replays the last run request" })
-			map("n", "<leader>hi", kulala.inspect, { desc = "inspect the current request" })
-			map("n", "<leader>hs", kulala.show_stats, { desc = "shows the statistics of the last run request" })
-			map("n", "<leader>ht", kulala.toggle_view, { desc = "toggles between the body and headers view" })
-			map("n", "<leader>hq", kulala.close, { desc = "close the kulala window" })
+			http.setup({})
+
+			map("n", "<leader>hr", "<cmd>HttpClient sendRequest<cr>", { desc = "runs the current request" })
 		end,
 	},
 }

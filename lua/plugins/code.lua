dae2edac74655cbcec5f81d9e398a3f27115bd6f
Rav4n-T <leonsgmali@gmail.com>
2024-09-23 19:11:20 +0800

    clear code plugins

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -92,19 +92,4 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"ray-x/go.nvim",
-	-- 	dependencies = { -- optional packages
-	-- 		"ray-x/guihua.lua",
-	-- 		"neovim/nvim-lspconfig",
-	-- 		"nvim-treesitter/nvim-treesitter",
-	-- 	},
-	-- 	ft = { "go", "gomod" },
-	-- 	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-	-- 	-- opts = {
-	-- 	-- 	verbose = true,
-	-- 	-- 	log_path = "/tmp/go.log",
-	-- 	-- },
-	-- 	config = true,
-	-- },
 }
--- lua/plugins/old/code.lua
+++ lua/plugins/old/code.lua
@@ -18,4 +18,30 @@ return {
 			require("codeium").setup({})
 		end,
 	},
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+		-- opts = {
+		-- 	verbose = true,
+		-- 	log_path = "/tmp/go.log",
+		-- },
+		config = true,
+	},
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+		config = function()
+			local rt = require("rust-tools")
+			rt.setup({
+				server = {},
+			})
+			rt.inlay_hints.enable()
+		end,
+	},
 }

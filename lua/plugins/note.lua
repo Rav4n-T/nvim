8af98aca3fad7b37517215f546466f8bdcc5ed82
Rav4n-T <leonsgmali@gmail.com>
2024-11-23 14:53:30 +0800

    add image.nvim to support show img in neovim

--- lua/plugins/note.lua
+++ lua/plugins/note.lua
@@ -39,4 +39,21 @@ return {
 		opts = {},
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	},
+	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			backend = "kitty",
+			processor = "magick_cli",
+			markdown = {
+				clear_in_insert_mode = true,
+				only_render_image_at_cursor = true,
+			},
+			max_width_window_percentage = 50,
+			max_height_window_percentage = 50,
+			window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+			editor_only_render_when_focused = true,
+			tmux_show_only_in_active_window = false,
+		},
+	},
 }

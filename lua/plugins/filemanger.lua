1efa3af427a786ea4feca4d75a4ac17246aa65e5
Rav4n-T <leonsgmali@gmail.com>
2024-11-23 14:53:41 +0800

    add oil.nvim

--- lua/plugins/filemanger.lua
+++ lua/plugins/filemanger.lua
@@ -65,4 +65,20 @@ return {
 			})
 		end,
 	},
+	{
+		"stevearc/oil.nvim",
+		keys = {
+			{
+				"<leader>o",
+				function()
+					require("oil").open_float()
+				end,
+				desc = "Explorer Oil (cwd)",
+			},
+		},
+		opts = {},
+		-- Optional dependencies
+		-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
 }

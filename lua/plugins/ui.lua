60e949c4d23f5f12d8b2b643d484f5d28ca3c05d
Rav4n-T <leonsgmali@gmail.com>
2024-11-23 14:50:44 +0800

    add indentscope plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,31 @@
 return {
 	{
+		"echasnovski/mini.indentscope",
+		event = { "BufEnter" },
+		opts = {
+			symbol = "╎",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+		config = function(_, opts)
+			require("mini.indentscope").setup(opts)
+		end,
+	},
+	{
+		"gen740/SmoothCursor.nvim",
+		event = "CursorMoved",
+		opts = {
+			fancy = {
+				enable = true,
+				head = { cursor = "", texthl = "SmoothCursor", linehl = nil },
+			},
+		},
 	},
 }

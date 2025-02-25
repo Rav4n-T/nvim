0a1e82218b3cad08a67cf0affd1dd27eac0bff53
Rav4n-T <leonsgmali@gmail.com>
2025-02-25 18:45:21 +0800

    add solidjs jsx class hide with treesitter

---
+++ after/queries/tsx/highlights.scm
@@ -0,0 +1,24 @@
+;; extends
+
+((jsx_attribute
+        (property_identifier) @att_name (#any-of? @att_name "className" "d")
+        (string (string_fragment) @class_value) (#set! @class_value conceal "…")
+        ))
+
+((jsx_attribute
+        (property_identifier) @att_name (#eq? @att_name "className")
+        (jsx_expression
+          (template_string (string_fragment) @string-fragment @class_value) (#set! @class_value conceal "…"))
+))
+
+((jsx_attribute
+        (property_identifier) @att_name (#any-of? @att_name "class" "d")
+        (string (string_fragment) @class_value) (#set! @class_value conceal "…")
+        ))
+
+((jsx_attribute
+        (property_identifier) @att_name (#eq? @att_name "class")
+        (jsx_expression
+          (template_string (string_fragment) @string-fragment @class_value) (#set! @class_value conceal "…"))
+))
+

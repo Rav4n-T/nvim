80a092749455daa2b8ee5dfd17413156c9d629ce
Rav4n-T <leonsgmali@gmail.com>
2025-02-25 18:50:08 +0800

    change nvim-tree to neo-tree

--- lua/plugins/filemanger.lua
+++ lua/plugins/filemanger.lua
@@ -1,66 +1,84 @@
 return {
 	{
-		"nvim-tree/nvim-tree.lua",
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
 		keys = {
 			{
 				"<leader>e",
 				function()
-					require("nvim-tree.api").tree.toggle({ path = vim.uv.cwd() })
+					require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
 				end,
-				desc = "Explorer NvimTree (cwd)",
+				desc = "Explorer NeoTree (cwd)",
 			},
 		},
+		deactivate = function()
+			vim.cmd([[Neotree close]])
+		end,
 		config = function()
-			local function my_on_attach(bufnr)
-				local api = require("nvim-tree.api")
-
-				local function opts(desc)
-					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-				end
-
-				-- default mappings
-				api.config.mappings.default_on_attach(bufnr)
-
-				-- custom mappings
-				vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-				vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
-				vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-			end
-
-			require("nvim-tree").setup({
-				on_attach = my_on_attach,
-				sort = {
-					sorter = "case_sensitive",
-				},
-				view = {
-					width = 30,
-				},
-				renderer = {
-					hidden_display = "all",
-					group_empty = true,
-					icons = {
-						git_placement = "right_align",
-						glyphs = {
-							modified = "󰣕 ",
-							git = {
-								untracked = "",
-								ignored = "",
-								unstaged = "",
-								staged = "",
-							},
-						},
+			require("neo-tree").setup({
+				close_if_last_window = false,
+				enable_git_status = true,
+				enable_diagnostics = true,
+				filesystem = {
+					bind_to_cwd = true,
+					follow_current_file = {
+						enabled = true,
 					},
 				},
-				git = {
-					show_on_open_dirs = false,
+				window = {
+					width = 30,
+					mappings = {
+						["<space>"] = "none",
+						["l"] = "open",
+						["L"] = "focus_preview",
+						["s"] = "open_split",
+						["S"] = "open_vsplit",
+					},
 				},
-				modified = {
-					enable = true,
-					show_on_open_dirs = false,
-				},
-				filters = {
-
-					dotfiles = true,
+				default_component_configs = {
+					indent = {
+						with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+						expander_collapsed = "",
+						expander_expanded = "",
+						expander_highlight = "NeoTreeExpander",
+					},
+					modified = {
+						symbol = "󰣕 ",
+						highlight = "NeoTreeModified",
+					},
+					name = {
+						trailing_slash = false,
+						use_git_status_colors = false,
+						highlight = "NeoTreeFileName",
+					},
+					diagnostics = {
+						symbols = {
+							error = " ",
+							warn = " ",
+							hint = " ",
+							info = " ",
+						},
+					},
+					git_status = {
+						symbols = {
+							-- Change type
+							added = " ",
+							modified = " ",
+							removed = " ",
+							deleted = "󰆴 ",
+							-- Status type
+							untracked = "",
+							ignored = "",
+							unstaged = "󰄱",
+							staged = "",
+							conflict = "",
+						},
+					},
 				},
 			})
 		end,
--- lua/plugins/old/filemanger.lua
+++ lua/plugins/old/filemanger.lua
@@ -1,84 +1,69 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	keys = {
-		{
-			"<leader>e",
-			function()
-				require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
-			end,
-			desc = "Explorer NeoTree (cwd)",
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("nvim-tree.api").tree.toggle({ path = vim.uv.cwd() })
+				end,
+				desc = "Explorer NvimTree (cwd)",
+			},
 		},
-	},
-	deactivate = function()
-		vim.cmd([[Neotree close]])
-	end,
-	config = function()
-		require("neo-tree").setup({
-			close_if_last_window = false,
-			enable_git_status = true,
-			enable_diagnostics = true,
-			filesystem = {
-				bind_to_cwd = true,
-				follow_current_file = {
-					enabled = true,
+		config = function()
+			local function my_on_attach(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- custom mappings
+				vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+				vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+				vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+			end
+
+			require("nvim-tree").setup({
+				on_attach = my_on_attach,
+				sort = {
+					sorter = "case_sensitive",
 				},
-			},
-			window = {
-				width = 30,
-				mappings = {
-					["<space>"] = "none",
-					["l"] = "open",
-					["L"] = "focus_preview",
-					["s"] = "open_split",
-					["S"] = "open_vsplit",
+				view = {
+					width = 30,
 				},
-			},
-			default_component_configs = {
-				indent = {
-					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-					expander_collapsed = "",
-					expander_expanded = "",
-					expander_highlight = "NeoTreeExpander",
+				renderer = {
+					hidden_display = "all",
+					group_empty = true,
+					icons = {
+						git_placement = "right_align",
+						glyphs = {
+							modified = "󰣕 ",
+							git = {
+								untracked = "",
+								ignored = "",
+								unstaged = "",
+								staged = "",
+							},
+						},
+					},
+				},
+				git = {
+					show_on_open_dirs = false,
 				},
 				modified = {
-					symbol = "󰣕 ",
-					highlight = "NeoTreeModified",
+					enable = true,
+					show_on_open_dirs = false,
 				},
-				name = {
-					trailing_slash = false,
-					use_git_status_colors = false,
-					highlight = "NeoTreeFileName",
+				filters = {
+
+					dotfiles = true,
 				},
-				diagnostics = {
-					symbols = {
-						error = " ",
-						warn = " ",
-						hint = " ",
-						info = " ",
-					},
-				},
-				git_status = {
-					symbols = {
-						-- Change type
-						added = " ",
-						modified = " ",
-						removed = " ",
-						deleted = "󰆴 ",
-						-- Status type
-						untracked = "",
-						ignored = "",
-						unstaged = "󰄱",
-						staged = "",
-						conflict = "",
-					},
-				},
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
